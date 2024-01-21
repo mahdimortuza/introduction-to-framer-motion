2426ca8a2c3ee8bdb5fa4e4fdcfaa99c16759269
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-21 12:42:11 +0600

    hover and tap

--- src/pages/Lesson1.tsx
+++ src/pages/Lesson1.tsx
@@ -1,30 +1,50 @@
 import { motion } from "framer-motion";
 const Lesson1 = () => {
+  // const parent = {
+  //   hidden: { opacity: 0, scale: 0.9 },
+  //   visible: { opacity: 1, scale: 1 },
+  // };
+
   const parent = {
     hidden: { opacity: 0, scale: 0.9 },
-    visible: { opacity: 1, scale: 1 },
-  };
-
-  const child = {
-    hidden: { opacity: 0, scale: 0.1 },
-    visible: { opacity: 1, scale: 1 },
+    visible: {
+      opacity: 0.6,
+      scale: 1,
+      transition: {
+        ease: "easeInOut",
+        duration: 0.5,
+      },
+    },
+    hover: {
+      opacity: 1,
+    },
   };
 
   return (
-    <div>
+    <div className="flex justify-center items-center h-[100vh] w-[1080px]">
       <motion.div
         className=" size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-4 p-5 justify-center items-center"
         variants={parent}
         initial="hidden"
         animate="visible"
-        transition={{
-          type: "easeInOut",
-          duration: 1.5,
-          delayChildren: 1,
-          staggerChildren: 1,
-        }}
+        whileHover="hover"
+        drag
       >
-        <motion.div
+        {/* <motion.div
+        className=" size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-4 p-5 justify-center items-center"
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+        whileTap="tap"
+        onHoverStart={() => {
+          console.log("hover");
+        }}
+        onHoverEnd={() => {
+          console.log("hover");
+        }}
+      > */}
+        {/* <motion.div
           variants={child}
           transition={{ type: "easeInOut", duration: 1.5 }}
           className="size-20 bg-cyan-400 rounded-sm"
@@ -43,7 +63,7 @@ const Lesson1 = () => {
           variants={child}
           transition={{ type: "easeInOut", duration: 1.5 }}
           className="size-20 bg-cyan-400 rounded-sm"
-        ></motion.div>
+        ></motion.div> */}
       </motion.div>
     </div>
   );
